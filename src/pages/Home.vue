00abed9dc353c6c540bde685f56dd07922482c5c
AsthuX <osm0348@naver.com>
2024-05-31 16:13:39 +0900

    fin

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h2 class="card-title">악동뮤지션을 소개합니다</h2>
+            <p class="card-text">0531 팀 과제입니다.</p>
+        </div>
+        <img
+            src="https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F009%2F2019%2F10%2F01%2F0004436602_001_20191001080317402.jpg&type=sc960_832"
+            alt=""
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+};
+</script>
