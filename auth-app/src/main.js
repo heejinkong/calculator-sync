a06ab8ba9d8bd116e065d9b88d3305f557e2946c
heejinKong <gmlwls3379@naver.com>
2024-05-31 16:47:57 +0900

    기본 틀 세팅

--- auth-app/src/main.js
+++ auth-app/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
