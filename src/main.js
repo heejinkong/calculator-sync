cf46c4f7265cd5851f55a0fff2155a9e71a72820
dh1010a <dh1010a@naver.com>
2024-06-21 15:11:29 +0900

    clean

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
